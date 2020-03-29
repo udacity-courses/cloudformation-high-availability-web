ecfe40684a1f4f83e061196adc1d835545caf77c
Anyul Rivas <anyul.rivas_oropeza@roche.com>
2020-03-29 17:06:17 +0200

    updating the servers' template

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,1 +1,2 @@
 aws cloudformation create-stack --stack-name MyStack --template-body file://../network.yaml  --parameters file://network-parameters.json
+aws cloudformation create-stack --stack-name MyStack --template-body file://../server.yaml  --parameters file://server-parameters.json --capabilities CAPABILITY_IAM
