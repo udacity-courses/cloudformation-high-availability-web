5d34f95a9239841b6b71006db850f97144ea99ea
Anyul Rivas <anyul.rivas_oropeza@roche.com>
2020-03-30 15:31:04 +0200

    adding new bash commands, updating the server template

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,2 +1,9 @@
-aws cloudformation create-stack --stack-name MyStack --template-body file://../network.yaml  --parameters file://network-parameters.json
-aws cloudformation create-stack --stack-name MyStack --template-body file://../server.yaml  --parameters file://server-parameters.json --capabilities CAPABILITY_IAM
+#!/usr/bin/env bash
+#aws cloudformation create-stack --stack-name MyStack --template-body file://../network.yaml  --parameters file://network-parameters.json
+#aws cloudformation create-stack --stack-name MyStack --template-body file://../server.yaml  --parameters file://server-parameters.json --capabilities CAPABILITY_IAM
+aws cloudformation create-stack \
+--stack-name "$1" \
+--template-body file://"$2" \
+--parameters file://"$3" \
+--region=eu-west-3 \
+--capabilities CAPABILITY_NAMED_IAM
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,1 +1,8 @@
-aws cloudformation update-stack --stack-name MyStack --template-body file://network.yaml  --parameters file://network-parameters.json
+#!/usr/bin/env bash
+#aws cloudformation update-stack --stack-name MyStack --template-body file://network.yaml  --parameters file://network-parameters.json
+export AWS_PROFILE=udacity
+aws cloudformation update-stack \
+--stack-name "$1" \
+--template-body file://"$2" \
+--parameters file://"$3" \
+--region=eu-west-3
